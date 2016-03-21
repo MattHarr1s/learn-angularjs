5aaed2997a7a21799292dc3913ca2ab555709815
Matt Harris <mateodelay@gmail.com>
2016-03-21 15:35:37 -0600

    Used $timeout to switch Tony to Everybody at 3000 ms.

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 var myApp = angular.module('myApp',[]);
 
 // CONTROLLERS
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope','$timeout', function($scope, $timeout) {
 
 		$scope.name='Tony';
+	$timeout(function(){
+		$scope.name = 'Everybody';
+	}, 3000);
 
 }]);
 
