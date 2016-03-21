d2922a9057abbd46eb6aae0ea17b153145742a71
Matt Harris <mateodelay@gmail.com>
2016-03-21 14:14:42 -0600

    Added ngMessages module to app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // MODULE
-var myApp = angular.module('myApp',[]);
+//Name of module goes in [], as seen below. For example ['ngMessages', 'ngMaps', 'etc']
+var myApp = angular.module('myApp',['ngMessages']);
 
 // CONTROLLERS
-myApp.controller('mainController', function($scope,$log, $filter) {
+myApp.controller('mainController', function($scope, $log, $filter) {
 
-	$log.log("Hello.");
-	$log.info("This is some information.");
-	$log.warn("Warning!");
-	$log.error("This was an Error!");
+	$scope.name ='John';
+	$scope.formattedname = $filter('uppercase')($scope.name);
+
+	$log.info($scope.name);
+	$log.info($scope.formattedname);
 });
 
 
