265ed65462ebbfed6324a83e734a6770bbdbdcea
Matt Harris <mateodelay@gmail.com>
2016-03-21 07:24:18 -0600

    Testing angular.injector

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var myApp = angular.module('myApp',[]);
 // CONTROLLERS
 myApp.controller('mainController', function($scope) {
 
-	console.log($scope);
-
 });
 
+var searchPeople = function searchPeople(firstName, lastName, height, age, occupation){
+	return 'Jane Doe';
+};
 
-
+console.log(angular.injector().annotate(searchPeople));
